1ed9993e4cfd2446c7be2212acee7bc77b7b39e9
Sura <s11819597@stu.najah.edu>
2022-12-11 15:56:41 +0200

    Retrive/edit

--- app.js
+++ app.js
@@ -130,6 +130,128 @@ app.post("/editTweet", (req,res)=>{
             });      
     });
 
+    //Retrieve all tweets (use paging) 
+app.post("/RetrieveTweet/:lastID", (req,res)=>{
+    const lastID = parseInt(req.params.lastID);
+    const ID = req.body.ID;
+    const Description = req.body.Description;
+            db.query("SELECT * FROM tweets LIMIT ? , 3 " ,[lastID],(err,result)=> {
+                if (err){
+                    console.log("Error !!");
+                }
+                // if there is no error --> 
+                console.log(result);
+                res.send(result);
+                
+            });      
+    });
+
+
+// Retrieve specific user tweets
+app.post("/RetrieveUser", (req,res)=>{
+    const UserId = req.body.UserId;
+            db.query("select * from tweets Where UserId = ?",[UserId],(err,result)=> {
+                if (err){
+                    console.log("Error !!");
+                }
+                // if there is no error --> 
+                console.log(result);
+                res.send(result);
+            });      
+    });
+
+
+//Retrieve user info
+app.post("/RetrieveUserInfo", (req,res)=>{
+    const ID = req.body.ID;
+            db.query("select ID,Username,FullName,Birthday,Address from users Where ID = ?",[ID],(err,result)=> {
+                if (err){
+                    console.log("Error !!");
+                }
+                // if there is no error --> 
+                console.log(result);
+                res.send(result);
+            });      
+    });
+
+
+//Edit my own info
+app.post("/editMyInfo", (req,res)=> {
+    const ID = req.body.ID;
+    const FullName = req.body.FullName;
+    const Birthday = req.body.Birthday;
+    const Address = req.body.Address;
+
+    const Username = req.body.Username;
+    const Password = req.body.Password;
+
+    //AUTHENTICATE LOGIN AND RETURN JWT TOKEN
+
+    db.query('SELECT * FROM users WHERE Username = ? and password = ?', [Username, Password], async (err, result) =>  {
+        if (err){
+            console.log("Error !!");
+        }
+        if (result.length > 0) {
+        
+            const accessToken = generateAccessToken ({user: req.body.Username})
+            const refreshToken = generateRefreshToken ({user: req.body.Username})
+            res.json ({accessToken: accessToken, refreshToken: refreshToken})
+
+            db.query("UPDATE users SET FullName = ?,Birthday = ?, Address =? WHERE ID = ?",[FullName,Birthday,Address,ID],(err,result)=> {
+                if (err){
+                    console.log("Error !!");
+                }
+                // if there is no error --> 
+                console.log("Your information has Edited");
+                res.send("information has Edited");
+            });      
+        
+          //  res.send('Welcome to Home page');
+          //  console.log("Welcome to Home page");
+        } else {
+            res.send('Incorrect Username and/or Password!');
+        }			
+    });
+});
+//Change password
+app.post("/changePass", (req,res)=>{
+    const ID = req.body.ID;
+    const Password = req.body.Password;
+    const Username = req.body.Username;
+
+    //AUTHENTICATE LOGIN AND RETURN JWT TOKEN
+
+    db.query('SELECT * FROM users WHERE Username = ? and password = ?', [Username, Password], async (err, result) =>  {
+        if (err){
+            console.log("Error !!");
+        }
+        if (result.length > 0) {
+        
+            const accessToken = generateAccessToken ({user: req.body.Username})
+            const refreshToken = generateRefreshToken ({user: req.body.Username})
+            res.json ({accessToken: accessToken, refreshToken: refreshToken})
+
+
+            db.query("UPDATE users SET Password= ? WHERE ID = ?",[Password,ID],(err,result)=> {
+                if (err){
+                    console.log("Error !!");
+                }
+                // if there is no error --> 
+                console.log("Password changed");
+                res.send("Password changed");
+            });
+        
+          //  res.send('Welcome to Home page');
+          //  console.log("Welcome to Home page");
+        } else {
+            res.send('Incorrect Username and/or Password!');
+        }			
+    });
+
+    
+
+                  
+    });
 
 app.listen('3000' , (err) => {
     if (err){
