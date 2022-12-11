dac3a3d0712b0a0531c9057b7150678f60cb4775
Sura <s11819597@stu.najah.edu>
2022-12-11 14:22:51 +0200

    SignUp

--- app.js
+++ app.js
@@ -25,7 +25,43 @@ db.connect((err)=>{
 }); 
 
 
-
+// Sign up 
+app.post("/adduser", (req,res)=>{
+    const ID = req.body.ID;
+    const Username = req.body.Username;
+    const FullName = req.body.FullName;
+    const Birthday = req.body.Birthday;
+    const Password = req.body.Password;
+    const Address = req.body.Address;
+    
+    // check if the user exist 
+    db.query('SELECT Username FROM users WHERE Username = ?', [Username], (err, result) =>  {
+        if (err){
+            console.log("Error !!");
+        }
+        // if there is no error --> 
+       else if( result.length > 0 ) {
+           
+                res.send('This username is already exist');
+                console.log("This username is already exist");
+        }
+        else {
+        
+            db.query("insert into users values (?,?,?,?,?,?)",[ID,Username,FullName,Birthday,Password,Address],(err,result)=> {
+                if (err){
+                    console.log("Error !!");
+                }
+                // if there is no error --> 
+                console.log("Result");
+                res.send("Add to users");
+            });
+        
+        }
+    });
+    
+    
+    });
+    
 app.listen('3000' , (err) => {
     if (err){
         throw err;
